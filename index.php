724857395ad0255b4c42538b6b8118e0ab5e8503
omarhaitham19 <omarhaithum19@gmail.com>
2024-02-11 20:27:50 +0200

    done

---
+++ index.php
@@ -0,0 +1,172 @@
+
+<?php require_once 'inc/header.php' ?>
+    <!-- Page Content -->
+    <!-- Banner Starts Here -->
+    <div class="banner header-text">
+      <div class="owl-banner owl-carousel">
+        <div class="banner-item-01">
+          <div class="text-content">
+            <!-- <h4>Best Offer</h4> -->
+            <!-- <h2>New Arrivals On Sale</h2> -->
+          </div>
+        </div>
+        <div class="banner-item-02">
+          <div class="text-content">
+            <!-- <h4>Flash Deals</h4> -->
+            <!-- <h2>Get your best products</h2> -->
+          </div>
+        </div>
+        <div class="banner-item-03">
+          <div class="text-content">
+            <!-- <h4>Last Minute</h4> -->
+            <!-- <h2>Grab last minute deals</h2> -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Banner Ends Here -->
+
+    <div class="latest-products">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2>Latest Posts</h2>
+              <!-- <a href="products.html">view all products <i class="fa fa-angle-right"></i></a> -->
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/product_01.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>Tittle goes here</h4></a>
+                <h6>created_at</h6>
+                <p> Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+                <!-- <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (24)</span> -->
+                <div class="d-flex justify-content-end">
+                  <a href="viewPost.php" class="btn btn-info "> view</a>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/product_02.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>Tittle goes here</h4></a>
+                <h6>created at</h6>
+                <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+                <!-- <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (21)</span> -->
+                <div class="d-flex justify-content-end">
+                  <a href="http://" class="btn btn-info "> view</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/product_03.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>Tittle goes here</h4></a>
+                <h6>created_at</h6>
+                <p>Sixteen Clothing is free CSS template provided by TemplateMo.</p>
+                <!-- <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (36)</span> -->
+                <div class="d-flex justify-content-end">
+                  <a href="http://" class="btn btn-info "> view</a>
+                </div>
+              </div>
+            </div>
+          </div>
+       
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/product_01.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>Tittle goes here</h4></a>
+                <h6>created_at</h6>
+                <p> Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+                <!-- <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (24)</span> -->
+                <div class="d-flex justify-content-end">
+                  <a href="http://" class="btn btn-info "> view</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/product_02.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>Tittle goes here</h4></a>
+                <h6>created at</h6>
+                <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+                <!-- <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (21)</span> --> 
+                <div class="d-flex justify-content-end">
+                  <a href="http://" class="btn btn-info "> view</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">  
+            <div class="product-item">
+              <a href="#"><img src="assets/images/product_03.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>Tittle goes here</h4></a>
+                <h6>created_at</h6>
+                <p>Sixteen Clothing is free CSS template provided by TemplateMo.</p>
+                <!-- <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (36)</span> -->
+                <div class="d-flex justify-content-end">
+                  <a href="http://" class="btn btn-info "> view</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+ 
+    
+<?php require_once 'inc/footer.php' ?>
---
+++ viewPost.php
@@ -0,0 +1,47 @@
+<?php require_once 'inc/header.php' ?>
+
+    <!-- Page Content -->
+    <div class="page-heading products-heading header-text">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="text-content">
+              <h4>new Post</h4>
+              <h2>add new personal post</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+    <div class="best-features about-features">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2>Our Background</h2>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="right-image">
+              <img src="assets/images/feature-image.jpg" alt="">
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="left-content">
+              <h4>Who we are &amp; What we do?</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed voluptate nihil eum consectetur similique? Consectetur, quod, incidunt, harum nisi dolores delectus reprehenderit voluptatem perferendis dicta dolorem non blanditiis ex fugiat. Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, consequuntur, modi mollitia corporis ipsa voluptate corrupti eum ratione ex ea praesentium quibusdam? Aut, in eum facere corrupti necessitatibus perspiciatis quis.</p>
+              
+              <div class="d-flex justify-content-center">
+                  <a href="editPost.php" class="btn btn-success mr-3 "> edit post</a>
+              
+                  <a href="deletePost.php" class="btn btn-danger "> delete post</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
+
+    <?php require_once 'inc/footer.php' ?>
