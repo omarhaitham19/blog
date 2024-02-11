c8df4d74db3a7835be466afbe47b70748747b827
omarhaitham19 <omarhaithum19@gmail.com>
2024-02-11 21:52:48 +0200

    done

---
+++ inc/connection.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "blog";
+
+$con = mysqli_connect($servername,$username,$password,$dbname);
+
+
+
+?>
