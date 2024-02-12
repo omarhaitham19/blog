4890613711a7d7f53b4af8f2af63f2bb494e6284
omarhaitham19 <omarhaithum19@gmail.com>
2024-02-13 01:15:40 +0200

    done

--- editPost.php
+++ editPost.php
@@ -22,10 +22,88 @@
       if (mysqli_num_rows($result) == 1) {
         $row = mysqli_fetch_assoc($result);
       }else{
-        // header("location:index.php");
+        header("location:index.php");
       }
     }else{
-      // header("location:index.php");
+      header("location:index.php");
+    }
+
+
+    if (isset($_POST['submit'])) {
+      function validate_input($input){
+        $input = trim($input);
+        $input = htmlspecialchars($input);
+        $input = stripcslashes($input);
+        return $input;
+    }
+
+    $title = mysqli_real_escape_string($con ,ucfirst(validate_input($_POST['title'])));
+    $body = mysqli_real_escape_string($con , validate_input($_POST['body']));
+    $img = $_FILES['image'];
+    $img_name = mysqli_real_escape_string($con , $_FILES['image']['name']);
+    $img_tmp = $_FILES['image']['tmp_name'];
+    $img_size = $_FILES['image']['size'];
+    $ImgExten =strtolower(pathinfo($img_name , PATHINFO_EXTENSION));
+    $extensions = ['jpg' , 'jpeg' , 'png'];
+    $dir = 'upload/';
+
+    $errors = [];
+
+    if (empty($title)) {
+        $errors['title'] = "title is required";
+    } elseif (is_numeric($title)) {
+        $errors['title'] = "Only letters and White space are allowed";
+      } 
+
+      if (empty($body)) {
+        $errors['body'] = "body is required";
+    } elseif (is_numeric($body)) {
+        $errors['body'] = "Only letters and White space are allowed";
+      } 
+
+      $query = "SELECT `id`,  `image` FROM `posts` WHERE `id` = $id";
+      $result = mysqli_query($con , $query);
+      if (mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_assoc($result);
+        $oldImage = $row['image'];
+
+        if (!empty($_FILES['image']['name'])) {
+          if(!in_array($ImgExten , $extensions)){
+            $errors['image'] = "Upload an image with an extension of jpg , jpeg or png";
+          }elseif($img_size > 5000000){
+            $errors['image'] = "Upload an image of maximum 5 megabyte";
+            }
+              $newName = uniqid() . "_" .  basename($img_name);
+            
+        }else{
+          $newName = $oldImage;
+        }
+
+        if (empty($errors)) {
+
+        $updateQuery = "UPDATE `posts` SET `title`='$title',`image`='$newName',`body`='$body' WHERE `id` = $id";
+        $runQuery = mysqli_query($con , $updateQuery);
+        if ($runQuery) {
+          if (!empty($_FILES['image']['name'])) {
+            unlink("upload/" . $oldImage);
+            move_uploaded_file($img_tmp , $dir . $newName);
+          }
+
+          echo "<script>alert('Post updated successfully!')</script>";
+          echo "<script>window.location.href='viewPost.php?id=$id';</script>";
+          exit();
+
+
+        }else{
+          echo "<script>alert('Error while updating')</script>";
+          echo "<script>window.location.href='index.php';</script>";
+          exit();
+        }
+
+        }
+      }else{
+        header("location:index.php");
+      }
     }
     ?>
 
@@ -42,16 +120,19 @@
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
             <input type="text" class="form-control" id="title" name="title" value="<?php echo isset($row['title']) ? $row['title'] : null ?>">
+            <span style="color: red;"> <?php echo isset($errors['title']) ? $errors['title'] : null ?> </span>
         </div>
         <div class="mb-3">
             <label for="body" class="form-label">Body</label>
             <textarea class="form-control" id="body" name="body" rows="5"><?php echo isset($row['body']) ? $row['body'] : null ?></textarea>
-        </div>
+            <span style="color: red;"> <?php echo isset($errors['body']) ? $errors['body'] : null ?> </span>
+          </div>
         <div class="mb-3">
             <label for="body" class="form-label">image</label>
             <input type="file" class="form-control-file" id="image" name="image" >
-        </div>
-        <!-- <img src="uploads/<?php echo $post['image'] ?>" alt="" width="100px" srcset=""> -->
+            <span style="color: red;"> <?php echo isset($errors['image']) ? $errors['image'] : null ?> </span>
+          </div>
+        <img src="upload/<?php echo $row['image'] ?>" alt="" width="100px" srcset="">
         <button type="submit" class="btn btn-primary" name="submit">Submit</button>
     </form>
 </div>
--- register.php
+++ register.php
@@ -1,3 +1,6 @@
+<?php
+require_once "inc/connection.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -114,19 +117,105 @@
     </div>
     <div>
     <?php
+
+$errors = [];
+function validate_input($input)
+{
+    $input = trim($input);
+    $input = htmlspecialchars($input);
+    $input = stripslashes($input);
+    return $input;
+}
+
+if (isset($_POST['submit'])) {
+
+    $name = mysqli_real_escape_string($con ,validate_input($_POST['name']));
+    $email = mysqli_real_escape_string($con ,validate_input($_POST['email']));
+    $password =mysqli_real_escape_string($con , validate_input($_POST['password'])); 
+    $phone = mysqli_real_escape_string($con , validate_input($_POST['phone']));
+
+    if (empty($name)) {
+        $errors['name'] = " Name is required";
+    } elseif (!ctype_alpha(str_replace(" ", "", $name))) {
+        $errors['name'] = "Only letters and White space are allowed";
+    }
+
+    if (empty($email)) {
+        $errors['email'] = "Email is required";
+    } elseif (filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL) === false) {
+        $errors['email'] = "Enter a valid Email";
+    }
+
+    if (empty($_POST['password'])) {   // we have to check the emptiness of the field by checking the field itself not the hashed 
+        $errors['password'] = "Password is required";
+    } elseif (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{3,}$/', $password)) {
+        $errors['password'] = "Password must include at least one uppercase letter, one lowercase letter, one number, and one special character";
+    }
+
+    if (empty($phone)) {
+        $errors['phone'] = "Phone is required";
+    } elseif (!preg_match('/^[0-9]{11}$/', $phone)) {
+        $errors['phone'] = "Enter a valid 10-digit phone number";
+    }
+
+
+    if (empty($errors)) {
+
+        $qy = "SELECT COUNT(*) AS count FROM users WHERE email = '$email'";
+        $checkPhone = "SELECT COUNT(*) AS count FROM users WHERE phone = '$phone'";
+        $rt = mysqli_query($con, $qy);
+        $rt2 = mysqli_query($con , $checkPhone);
+        $row = mysqli_fetch_assoc($rt);
+        $row2 = mysqli_fetch_assoc($rt2);
+        $emailCount = $row['count'];
+        $phoneCount = $row2['count'];
+
+        if ($emailCount > 0) {
+        echo '<script language="javascript">';
+        echo 'alert("This email already exists!")';
+        echo '</script>';
+      } elseif($phoneCount > 0){
+        echo '<script language="javascript">';
+        echo 'alert("This Phone Number already exists!")';
+        echo '</script>';
+      }  else {
+
+        $hashed_password = password_hash($password , PASSWORD_DEFAULT);
+
+        $query = "INSERT INTO `users`(`name`, `email`, `password`, `phone`)
+         VALUES('$name' ,'$email' , '$hashed_password' , '$phone')";
+
+         if (mysqli_query($con , $query)) {
+            echo "<script>alert('Your account created successfully!')</script>";
+            echo "<script>window.location.href='login.php';</script>";
+            exit();  
+         } else {
+            echo "<script> alert('An Error Occured!') </script>";
+         }
+
+    } 
+}
+}
+
+
+
+
+
+
 ?>
 
         <form class="form" action="" method="post">
             
             <h3>Register Here</h3>
             <input placeholder="Enter Name" class="input" type="text" name="name" id=""value="">
+            <span style="color: red;"> <?php echo isset($errors['name']) ? $errors['name'] : null ?> </span>
             <input placeholder="Enter Email" class="input" type="email" name="email" id=""value="">
+            <span style="color: red;"> <?php echo isset($errors['email']) ? $errors['email'] : null ?> </span>
             <input class="input" placeholder="Enter Password" type="password" name="password" id="">
+            <span style="color: red;"> <?php echo isset($errors['password']) ? $errors['password'] : null ?> </span>
             <input class="input" placeholder="Enter your phone " type="text" name="phone" id="">
+            <span style="color: red;"> <?php echo isset($errors['phone']) ? $errors['phone'] : null ?> </span>
             <button type="submit" name="submit">Register</button>
-           
-            
-
         </form>
     </div>
 </body>
